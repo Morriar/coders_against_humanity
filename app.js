a01468f0857abf5ded8470ec438d0adc34596c7f
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-21 01:28:01 -0500

    git: add copyrigths
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,8 +21,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session')
-
-//var auth = require('./middlewares/auth.js');
 
 var routes = require('./routes/index');
 
