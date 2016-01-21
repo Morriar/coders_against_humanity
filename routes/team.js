27d4238adc749775e0011f2d49e90292d3891d26
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-20 23:07:07 -0500

    team: merge views with the main ones
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/team.js
+++ routes/team.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET wait teams. */
 router.get('/', function(req, res, next) {
-  res.render('team/index', {});
+  res.render('join_room', {});
 });
 
 /* next is show scores */
 router.get('/wait_teams', function(req, res, next) {
-  res.render('team/wait_teams', {});
+		res.render('wait_teams', { session: req.session, room: room});
 });
 
 /* next is white_cards or end game */
 router.get('/show_scores', function(req, res, next) {
-  res.render('team/show_scores', {});
+		res.render('show_scores', { session: req.session, room: room});
 });
 
 /* next is show results */
 router.get('/show_white_cards', function(req, res, next) {
-  res.render('team/show_white_cards', {});
+		res.render('show_white_cards', { session: req.session, room: room, team: team});
 });
 
 /* next is show scores */
 router.get('/show_results', function(req, res, next) {
-  res.render('team/show_results', {});
+		res.render('show_results', { session: req.session, room: room});
 });
 
 /* next is home */
 router.get('/end_game', function(req, res, next) {
-  res.render('team/end_game', {});
+		res.render('end_game', { session: req.session, room: room});
 });
 
 module.exports = router;
