bf2c638884c6abd90d00187796b450a75f111339
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-21 00:30:37 -0500

    team: login and wait for other teams
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/team.js
+++ routes/team.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var cards = require('../model/cards');
+var rooms = require('../model/rooms');
 var router = express.Router();
 
 /* GET wait teams. */
@@ -6,9 +8,41 @@ router.get('/', function(req, res, next) {
   res.render('join_room', {});
 });
 
+router.post('/join_room', function(req, res, next) {
+	var room_id = req.body.room;
+	var team_name = req.body.team;
+
+	if(!room_id || !team_name) {
+		res.redirect('/team');
+		return;
+	}
+	rooms.findOne(room_id, function(room) {
+		if(!room) {
+			res.redirect('/team');
+			return;
+		}
+		var team = {
+			name: team_name,
+			score: 0,
+			hand: []
+		}
+		req.session.room_id = room_id;
+		req.session.team_name = team_name;
+		room.teams[team_name] = team
+		rooms.save(room);
+		res.redirect('/team/wait_teams');
+	});
+});
+
 /* next is show scores */
 router.get('/wait_teams', function(req, res, next) {
+	if(!req.session.room_id || !req.session.team_name) {
+		res.redirect('/');
+		return;
+	}
+	rooms.findOne(req.session.room_id, function(room) {
 		res.render('wait_teams', { session: req.session, room: room});
+	});
 });
 
 /* next is white_cards or end game */
