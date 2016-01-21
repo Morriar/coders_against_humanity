643617bd6493fd00b471f4092aada1c23aead5de
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-20 20:59:34 -0500

    model: introduce rooms
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ model/rooms.js
@@ -0,0 +1,44 @@
+//  Copyright 2015 Alexandre Terrasa <alexandre@moz-code.org>.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+var db = require('mongoskin').db('mongodb://localhost:27017/csg_cah');
+db.bind('rooms');
+
+exports.find = function(req, callback) {
+	db.rooms.find(req).toArray(function(err, rooms) {
+		if(err) {
+			console.log(err);
+			rooms = [];
+		}
+		callback(rooms);
+	});
+}
+
+exports.findOne = function(room_id, callback) {
+	db.rooms.findOne({id: room_id}, function(err, room) {
+		callback(room);
+	});
+}
+
+exports.save = function(room) {
+	db.rooms.update({'id': room.id}, room);
+}
+
+exports.insert = function(room) {
+	db.rooms.insert(room);
+}
+
+exports.remove = function(room) {
+	db.rooms.remove({id: room.id});
+}
